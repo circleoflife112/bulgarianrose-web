3f7acc15914b791410f819b6a9a0f1a2ed49f1d5
Yuri Han <yurihan77@google.com>
2025-02-05 15:54:34 +0900

    change date

--- js/logo.js
+++ js/logo.js
@@ -1,8 +1,3 @@
-const navHolder = document.getElementById("main_nav_holder");
-const mainNav = document.getElementById("main_nav");
-const logoHolder = document.getElementById("bulgarianrose_logo");
-const bulgarianLogo = document.querySelector("#bulgarianrose_logo a");
-
 const hamburgerBtnHolder = document.querySelectorAll(".hamburger_menu");
 const hamburgerBtn = document.querySelectorAll(".hamburger_menu a");
 const asideNavi = document.getElementById("aside_nav");
