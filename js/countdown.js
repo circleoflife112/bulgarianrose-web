72a5bd0603dfb2b476806c0b65b7f484a09c596e
Yuri Han <yurihan77@google.com>
2026-01-10 16:52:07 +0900

    correct date

--- js/countdown.js
+++ js/countdown.js
@@ -6,7 +6,7 @@
 // setInterval(getClock, 1000);
 
 // Set the date we're counting down to
-const countDownDate = new Date("December 31, 2025 23:59:59").getTime();
+const countDownDate = new Date("March 31, 2026 23:59:59").getTime();
 
 // Update the count down every 1 second
 const x = setInterval(function () {
